5a12b8afae3b6d112afa4362d335161c0f762557
Ryan Harrington <krooked590@gmail.com>
2019-09-17 12:21:37 -0700

    added another env var

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ var Customer = require('./Customer');
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        "project_id": "mikes-pro-shop",
+        "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     }),
